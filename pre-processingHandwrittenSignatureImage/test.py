b796596cfb8dd5805f93cb422a0a821077a5280d
Chazelam <andreilaymzin@gmail.com>
2022-12-16 11:32:13 +0300

    tests

--- pre-processingHandwrittenSignatureImage/test.py
+++ pre-processingHandwrittenSignatureImage/test.py
@@ -1,4 +1,11 @@
 from PIL import Image
+import numpy as np
 
+orig = Image.open("signatureExample(CEDAR 21)/original_21_1.png")
+new = orig.convert("RGB")
+r, g, b = new.split()
 
+r = r.save("r.png")
+g = g.save("g.png")
+b = b.save("b.png")
 print("All work!")
