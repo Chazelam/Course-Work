ed511a2516190bd723c627302067a1fd5d9b495a
Chazelam <andreilaymzin@gmail.com>
2022-12-18 14:47:46 +0300

    Скрипт для поиска среднего разрешения подписи

---
+++ pre-processingHandwrittenSignatureImage/resolution.py
@@ -0,0 +1,27 @@
+# Задача: 
+#  Пройтись по всем изображениям
+#   посчитать
+#    1. макс. х , макс у
+#     2. ср. значения
+
+import cv2 as cv
+
+def avrg(x):
+    return int(sum(x)/len(x))
+
+
+Ys = []
+Xs = []
+n = 23
+for i in range(1, n + 1):
+    for j in range(1, 25):
+        # Открытие файла
+        file_name = "/root/VScode/NeuralNetworks/new_data/{0}/original_{1}_{2}.png".format(i, i, j)
+        img = cv.imread(file_name)
+        Y, X, Channels = img.shape
+        Ys.append(Y)
+        Xs.append(X)
+
+print("max x is ", max(Xs))
+print("max y is ", max(Ys))
+print("avrg is {0}x{1}".format(avrg(Xs), avrg(Ys)))
